416e6a460369a876dbeeb23c05e284d3f3a97d31
Karam Issa <karamissa79@gmail.com>
2021-11-29 21:54:59 +0300

    Flesh out the event system for User

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
-console.log(user);
 
-user.set({ name: 'Karam', age: 21 });
-console.log(user);
+user.on('change', () => {
+  console.log('Added!');
+});
+
+user.trigger('change');
