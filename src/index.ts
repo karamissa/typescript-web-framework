f95230bf385f9f53285b182e5e8b2f72cd21f7d7
Karam Issa <karamissa79@gmail.com>
2021-12-10 16:54:04 +0300

    Create Collection View class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { User } from './models/User';
-import { UserEdit } from './views/UserEdit';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'Dan', age: 27 });
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById('root');
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
 
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found!');
-}
+users.fetch();
